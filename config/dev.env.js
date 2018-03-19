495fbaae4aa7736cc190f0a6d66f41f86508c9c6
antoinefa <fabien.antoine@m4x.org>
2018-03-19 15:35:37 +0100

    Make/docker : added developpement environnement

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  ES_HOST: '"http://localhost:9200"',
   PATH: '"/av/test"',
   indices: {
     acc: '"es5dev_2005_2015_accidents"',
